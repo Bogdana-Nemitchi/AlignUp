99c4bbed0810223039f79c48bd103a215c8a92d6
Andriana Golban <andriana.golbana@isa.utm.md>
2025-05-26 17:50:01 +0300

    Rezolvarea parțială a problemei de auth. urmează rezolvara problemei cu baza de date

--- AlignUp.Domain/Model/User/UserRegisterResponseDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AlignUp.Domain.Model.User
-{
-    public class UserRegisterResponseDTO
-    {
-        public bool Status { get; set; }
-        public string StatusMessage { get; set; }
-    }
-}
--- AlignUp.Web/App_Start/RouteConfig.cs
+++ AlignUp.Web/App_Start/RouteConfig.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-3
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 namespace AlignUp.Web
 {
     public class RouteConfig
